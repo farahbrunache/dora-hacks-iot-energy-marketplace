a9c73c1ddfb309e31e5cee39ae413dc9b7b8d8df
niickchen <niickchen@users.noreply.github.com>
2018-06-27 05:19:58 -0700

    Add elements to profile and dashboard

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Chart from 'chart.js'
 
 class Dashboard extends Component {
   constructor(props, { authData }) {
@@ -6,14 +7,59 @@ class Dashboard extends Component {
     authData = this.props
   }
 
+  componentDidMount() {
+    const chart = document.getElementById('mychart').getContext('2d');
+    const data = {
+      labels: ['Jun 22', 'Jun 23', 'Jun 24', 'Jun 25', 'Jun 26', 'Jun 27'],
+      fillColor: ["rgba(0,10,220,0.5)","rgba(220,0,10,0.5)","rgba(220,0,0,0.5)","rgba(120,250,120,0.5)" ],
+            strokeColor: "rgba(220,220,220,0.8)", 
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+        datasets: [{
+            label: '# of Votes',
+            data: [5.0, 4.6, 4.9, 4.78, 5.0, 4.97],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+    new Chart(chart).Line(data);
+  }
+
   render() {
+    
+
+    const options = {
+      scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    }};
+
     return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
+      <main className='container'>
+        <div className='pure-g'>
+          <div className='pure-u-1-1'>
             <h1>Dashboard</h1>
-            <p><strong>Congratulations {this.props.authData.name}!</strong> If you're seeing this page, you've logged in with UPort successfully.</p>
-          </div>
+            <canvas id='mychart'/>
+            <img src='https://s3-us-west-1.amazonaws.com/random-stuff-guess/dorahacks/dashboard.png' alt=''/>
+            </div>
         </div>
       </main>
     )
--- src/user/layouts/profile/Profile.js
+++ src/user/layouts/profile/Profile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import {Doughnut as DoughnutChart} from 'react-chartjs';
 
+import './Profile.css'
 
 class Profile extends Component {
   constructor(props, { authData }) {
@@ -13,21 +15,35 @@ class Profile extends Component {
   }
 
   render() {
+    const chartData = {
+      datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+    };
+
+    const chartOptions = {
+    };
+    
     return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
-            <p>
+      <main className='container'>
+        <div className='pure-g'>
+          <div className='pure-u-1-1'>
+            <p id='name'>
               {this.props.authData.name}
             </p>
-            <p>
-              {this.props.authData.address}
-            </p>
-          <div class="pure-u-1-1" id='myChart'>
-          </div>
-          <p>628 kWh Produced</p>
-          <p>You saved 58.2 dollars</p>
-          <Link to="/dashboard" id='detailButton' className="pure-u-1-1 pure-menu-link">Details</Link>
+            <hr/>
+          <img src='https://s3-us-west-1.amazonaws.com/random-stuff-guess/dorahacks/ring.png' alt=''/>
+          <p id='energy'>628 kWh</p>
+          <p id='produced'>Produced</p>
+          <p>You saved 70.2 dollars</p>
+          <Link to='/dashboard' id='detailButton' className='pure-u-1-1 pure-menu-link'>Details</Link>
           </div>
         </div>
       </main>
